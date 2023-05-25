5b7a769a3370f148e7747136a46acb9946de4eae
huaanth <115844975+huaanth@users.noreply.github.com>
2023-05-24 22:48:00 -0400

    Update GUI.py
    pygame will run and display sudoku

--- GUI.py
+++ GUI.py
@@ -13,3 +13,19 @@ class Grid:
         self.width = width
         self.height = height
         self.win = win
+
+        
+def main():
+    win = pygame.display.set_mode((540,600))
+    pygame.display.set_caption("Sudoku")
+    board = Grid(9,9,540,540,win)
+    key = None
+    run = True
+    start_time = time.time()
+    wrong =0
+    while run:
+        play_time = round(time.time()- start_time)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pass #continue tomorrow
