7bc3e04d678725df93b6ea3170dfd57d459164cc
huaanth <115844975+huaanth@users.noreply.github.com>
2023-05-26 12:14:52 -0400

    Update GUI.py
    Added a Box class, so it can draw and input the numbers in the selected boxes. Also put in the keyboard and keypad features for the numbers (1-9) so the user can input either or.

--- GUI.py
+++ GUI.py
@@ -14,6 +14,18 @@ class Grid:
         self.height = height
         self.win = win
 
+class Box:
+    #will draw the stuff in the box
+    rows = 9
+    cols = 9
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row= row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False        
         
 def main():
     win = pygame.display.set_mode((540,600))
@@ -28,4 +40,41 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        pass #continue tomorrow
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key=1
+                if event.key == pygame.K_2:
+                    key=2
+                if event.key == pygame.K_3:
+                    key=3
+                if event.key == pygame.K_4:
+                    key=4
+                if event.key == pygame.K_5:
+                    key=5
+                if event.key == pygame.K_6:
+                    key=6
+                if event.key == pygame.K_7:
+                    key=7
+                if event.key == pygame.K_8:
+                    key=8
+                if event.key == pygame.K_9:
+                    key=9
+                if event.key == pygame.K_KP1:
+                    key=1
+                if event.key == pygame.K_KP2:
+                    key=2
+                if event.key == pygame.K_KP3:
+                    key=3
+                if event.key == pygame.K_KP4:
+                    key=4
+                if event.key == pygame.K_KP5:
+                    key=5
+                if event.key == pygame.K_KP6:
+                    key=6
+                if event.key == pygame.K_KP7:
+                    key=7
+                if event.key == pygame.K_KP8:
+                    key=8
+                if event.key == pygame.K_KP9:
+                    key=9
+        
